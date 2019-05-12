87f10da00f1b859a3621c8fd378b69ec047e4243
zhoulu1201 <1261084965@qq.com>
2019-05-12 22:08:49 +0800

    lol更新

--- mains/node/config/server.js
+++ mains/node/config/server.js
@@ -1,6 +1,6 @@
 module.exports={
   local:{
-    open:true,
+    open:false,
     port:3000
   },
   net:{
@@ -8,7 +8,7 @@ module.exports={
     port:8080,
   },
   http:{
-    open:false,
+    open:true,
     port:80
   },
   https:{
---
+++ mains/zhoulu/src/components/loading-z.vue
@@ -0,0 +1,11 @@
+<template>
+    <van-loading type="spinner" size="60px"  class="loading"/>
+</template>
+<script>
+
+</script>
+
+<style>
+.loading{margin:auto;}
+</style>
+
